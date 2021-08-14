c67e21833ef3650d6bc958f02ff9e1f3b369f505
ashramen <ashrahman18@gmail.com>
2021-08-14 18:59:04 -0400

    add slash commands

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    key: {
-      token: process.env.DISCORDJS_BOT_TOKEN,
-      client_id: process.env.DISCORDJS_CLIENT_ID,
-      guild_id: process.env.DISCORDJS_GUILD_ID,
-    }
+	token: process.env.DISCORDJS_BOT_TOKEN,
+	client_id: process.env.DISCORDJS_CLIENT_ID,
+	guild_id: process.env.DISCORDJS_GUILD_ID,
 };
