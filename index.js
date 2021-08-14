59ba1cb9474742ef8dcf85848b2be062b43d7898
ashramen <ashrahman18@gmail.com>
2021-08-14 19:01:52 -0400

    change ready event to once

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const rest = new REST({ version: '9' }).setToken(keys.token);
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.on('ready', () => {
+client.once('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
