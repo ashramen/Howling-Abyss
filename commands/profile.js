209fbc974573c3852f3c20e7fa9ae5ccb287b806
ashramen <ashrahman18@gmail.com>
2021-08-15 19:06:30 -0400

    add dynamic thumbnail logic

--- commands/profile.js
+++ commands/profile.js
@@ -1,42 +1,37 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const summoner = require('../models/summonerSchema');
 const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('profile')
     .setDescription('View your summoner profile'),
   async execute(interaction) {
-    summoner.findOne(
+    const version = await fetch(
+      'https://ddragon.leagueoflegends.com/api/versions.json',
+    )
+      .then((response) => response.json())
+      .then((response) => response[0]);
+    var retEmbed;
+    await summoner.findOne(
       { userId: interaction.user.id },
-      function (err, existingUser) {
+      async function (err, existingUser) {
         if (err) return;
         if (!existingUser) {
-          return interaction.reply('Use /summoner add to add your account!');
+          await interaction.reply('Use /summoner to add your account!');
+          return;
         } else {
-          const retEmbed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle(`✨Summoner Profile: ${existingUser.summonerName}`)
+          retEmbed = new MessageEmbed()
+            .setTitle(`✨Summoner: ${existingUser.summonerName}`)
             .setDescription('Welcome to the Howling Abyss!')
-            .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-            .addFields(
-              { name: 'Regular field title', value: 'Some value here' },
-              { name: '\u200B', value: '\u200B' },
-              {
-                name: 'Inline field title',
-                value: 'Some value here',
-                inline: true,
-              },
-              {
-                name: 'Inline field title',
-                value: 'Some value here',
-                inline: true,
-              },
-            )
-            .setImage('https://i.imgur.com/AfFp7pu.png');
-          return interaction.channel.send({ embeds: [retEmbed] });
+            .setColor('#87ceeb')
+            .setThumbnail(
+              `http://ddragon.leagueoflegends.com/cdn/${version}/img/profileicon/${existingUser.summonerProfileIconId}.png`,
+            );
         }
       },
     );
+    await interaction.reply({ embeds: [retEmbed] });
   },
 };
--- commands/summoner.js
+++ commands/summoner.js
@@ -11,7 +11,7 @@ module.exports = {
       option.setName('name').setDescription('summoner name').setRequired(true),
     ),
   async execute(interaction) {
-    const summonerName = interaction.options.getString('name');
+    let summonerName = interaction.options.getString('name');
     const reqURL = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${keys.riot_key}`;
     const summonerData = await fetch(reqURL)
       .then((response) => response.json())
@@ -23,24 +23,20 @@ module.exports = {
       await interaction.reply('Invalid summoner name 😦');
       return;
     }
-    const summonerId = await summonerData.id;
-    const summonerPuuid = await summonerData.puuid;
-    const summonerLevel = await summonerData.summonerLevel;
-    const summonerProfileIconId = await summonerData.profileIconId;
-
     let query = { userId: interaction.user.id },
       update = {
         serverId: interaction.guildId,
-        summonerName: summonerName,
-        summonerId: summonerId,
+        summonerName: summonerData.name,
+        summonerId: summonerData.id,
         summonerPuuid: summonerPuuid,
-        summonerLevel: summonerLevel,
-        summonerProfileIconId: summonerProfileIconId,
+        summonerLevel: summonerData.summonerLevel,
+        summonerProfileIconId: summonerData.profileIconId,
       },
       options = { upsert: true, new: true };
 
     summoner.findOneAndUpdate(query, update, options, function (error, result) {
       if (error) return;
+      result.save();
     });
     await interaction.reply(`Your summoner name has been saved! 😄`);
   },
