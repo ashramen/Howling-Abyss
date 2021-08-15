19be7a36d56104eca9030021e3847f7a02237bb8
ashramen <ashrahman18@gmail.com>
2021-08-15 14:24:33 -0400

    refactor mongoose connect

--- dev/champ.js
+++ dev/champ.js
@@ -1,12 +1,3 @@
 const mongoose = require('mongoose');
 const keys = require('../config');
-
-mongoose
-	.connect(keys.mongodb_srv, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-		useCreateIndex: true,
-	})
-	.then(() => console.log('Connected to database'))
-	.catch((err) => console.log(err));
+const summoner = require('../models/summonerSchema');
